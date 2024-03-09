e5bf7d94d40991aa4ee0d93aaff627f280836d73
H.Meneses <hm_kali@Matrix.com.br>
2024-03-09 06:27:47 -0300

    Hello Neo

---
+++ AdmSys/cp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Scripting by H.Meneses
+#1. To copy a file to another location:
+echo 'File name to copy:'
+read file_name
+echo 'Destination:'
+read destination
+cp file_name destination
